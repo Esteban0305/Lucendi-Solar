baa94eec7a5e76bb4a523cd922398c80ada3e3f6
Esteban Muñoz <teban0305@hotmail.com>
2021-07-23 10:33:50 -0500

    Update

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let Slides = [
 	"src/Slide 1.jpeg",
 	"src/Slide 2.jpeg",
-	"src/Slide 3.jpeg"
+	"src/Slide 3.jpeg",
+	"src/Slide 4.jpeg"
 ];
 px = (Slides.length-1)*1092;
 $(document).ready(function(){
@@ -19,7 +20,53 @@ $(document).ready(function(){
 			last = $(".slideshow .slide:first-child");
 			$(".slideshow").append(last);
 			$(".slideshow .slide:last-child").css({"left": px + "px"}, 0);
-			console.log(px);
 		}, 600);
 	},5000);
 });
+
+$(".productos .marca span").click(function(){
+	$(this).next().slideToggle(500);
+	if($(this).text() == "expand_more"){
+		$(this).text("expand_less");
+	} else {
+		$(this).text("expand_more");
+	}
+});
+
+function SendMSG(){
+	event.preventDefault();
+	form = $("form").serializeArray();
+	Nombre = form[0]["value"];
+	Tel = form[1]["value"];
+	Email = form[2]["value"];
+	Mensaje = form[3]["value"];
+
+	message = "Nombre: " + Nombre + "\nTeléfono: " + Tel + "\nEmail: " + Email + "\nMensaje: " + Mensaje;
+	telegram_bot_id = "1919704084:AAHf8H0iwdyAr7g8AWScA6sUR88I-6YEKNw";
+	chat_id = "1336620580";
+	var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+		"method": "POST",
+		"headers": {
+			"Content-Type": "application/json",
+		    "cache-control": "no-cache",
+		    "Access-Control-Allow-Methods":"GET,POST,OPTIONS"
+		},
+		"data": JSON.stringify({
+		"chat_id": chat_id,
+		"text": message
+		})
+	}
+
+	$.ajax(settings).done(function (res) {
+	  if(res["ok"] == true){
+	  	$("form").trigger("reset");
+	  	$("form").append("<div class='alert alertSuccess'>Mensaje Enviado</div>");
+	  } else {
+	  	$("form").append("<div class='alert alertError'>Error al enviar el mensaje</div>");
+	  }
+	  res = "";
+	}); 
+}
